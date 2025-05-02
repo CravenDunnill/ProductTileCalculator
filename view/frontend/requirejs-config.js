d7037a6303fbdcd2ce56c956e02a819bca290365
Justin Taylor <jt@justintaylor.org>
2025-05-02 08:40:44 +0100

    Remove any Cart and Checkout changes

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,9 +1,7 @@
 var config = {
 	map: {
 		'*': {
-			'tile-calculator': 'CravenDunnill_ProductTileCalculator/js/tile-calculator',
-			'cart-calculator': 'CravenDunnill_ProductTileCalculator/js/cart-calculator',
-			'box-price-update': 'CravenDunnill_ProductTileCalculator/js/box-price-update'
+			'tile-calculator': 'CravenDunnill_ProductTileCalculator/js/tile-calculator'
 		}
 	}
 };
